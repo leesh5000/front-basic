abfe911c96432c28d167bc9f4564c7083994394e
leesh5000 <leesh50000@gmail.com>
2022-02-26 19:22:48 +0900

    initial commit

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ScrollBox from "./ScrollBox";
+import {Component} from "react";
+import IterationSample from "./IterationSample";
+
+class App extends Component {
+    render() {
+        return (
+            <IterationSample/>
+        )
+    }
+}
+
+export default App;
