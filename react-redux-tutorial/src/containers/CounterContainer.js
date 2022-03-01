c2de6628fe8e08047ae2222bb44b1a03864b863a
leesh5000 <leesh50000@gmail.com>
2022-03-01 22:20:12 +0900

    react-redux finish commit

---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,20 @@
+import Counter from "../components/Counter";
+import {useDispatch, useSelector} from "react-redux";
+import {decrease, increase} from "../modules/counter";
+import {useCallback} from "react";
+
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    return (
+        <Counter
+            number={number}
+            onIncrease={onIncrease}
+            onDecrease={onDecrease}
+        />
+    );
+};
+
+export default CounterContainer;
---
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -0,0 +1,30 @@
+import Todos from "../components/Todos"
+import {useSelector} from "react-redux";
+import {changeInput, insert, remove, toggle} from "../modules/todos";
+import React, {memo} from "react";
+import useActions from "../lib/useActions";
+
+const TodosContainer = () => {
+    const {input, todos} = useSelector(({todos}) => ({
+        input: todos.input,
+        todos: todos.todos
+    }));
+
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+        [changeInput, insert, toggle, remove],
+        []
+    );
+
+    return (
+        <Todos
+            input={input}
+            todos={todos}
+            onChangeInput={onChangeInput}
+            onInsert={onInsert}
+            onToggle={onToggle}
+            onRemove={onRemove}
+        />
+    );
+};
+
+export default memo(TodosContainer);
