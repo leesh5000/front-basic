0176a6300db4dc0df8935565da4c4d0460a8a7d1
leesh5000 <leesh50000@gmail.com>
2022-03-01 16:38:05 +0900

    news-viewer finish commit

---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import usePromise from "../lib/usePromise";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+    title: '제목',
+    description: '내용',
+    url: 'https://google.com',
+    urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = ({category}) => {
+
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=dae662bdc6f345108409a5fe343b6465`);
+    }, [category]);
+
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>;
+    }
+
+    if (!response) {
+        return null;
+    }
+
+    if (error) {
+        return <NewsListBlock>에러 발생</NewsListBlock>
+    }
+
+    const {articles} = response.data;
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article}/>
+            ))}
+        </NewsListBlock>
+    );
+};
+
+export default NewsList;
---
+++ router-tutorial/src/pages/About.js
@@ -0,0 +1,30 @@
+import {useLocation, useSearchParams} from "react-router-dom";
+
+const About = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const detail = searchParams.get('detail');
+    const mode = searchParams.get('mode');
+
+    const onToggleDetail = () => {
+        setSearchParams({detail: detail !== 'true', mode});
+    }
+
+    const onIncreaseMode = () => {
+        const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+        setSearchParams({detail, mode: nextMode});
+    }
+
+    const location = useLocation();
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>리액트 라우터를 사용해보는 프로젝트 입니다.</p>
+            <p>detail: {detail}</p>
+            <p>mode: {mode}</p>
+            <button onClick={onToggleDetail}>Toggle detail</button>
+            <button onClick={onIncreaseMode}>mode + 1</button>
+        </div>
+    )
+}
+
+export default About;
